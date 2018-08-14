203258a4aaddd8323d4da9d11c7ebf4d7d4194af
Dustin Morrill <dmorrill10@gmail.com>
2018-08-13 21:20:23 -0600

    Add methods to `PlateauFunction`.

--- robust_offline_contextual_bandits/robust_offline_contextual_bandits/plateau_function.py
+++ robust_offline_contextual_bandits/robust_offline_contextual_bandits/plateau_function.py
@@ -1,4 +1,6 @@
 import numpy as np
+from robust_offline_contextual_bandits.data import DataComponentsForTraining
+from tf_supervised_inference.data import Data, NamedDataSets
 
 
 class PlateauFunction(object):
@@ -29,5 +31,38 @@ class PlateauFunction(object):
                 self.heights[i], stddev, size=[num_bounded_x])
         return y
 
+    def in_bounds(self, x):
+        in_bounds = np.full([x.shape], False)
+        for i in range(len(self.x_bounds)):
+            x_min, x_max = self.x_bounds[i]
+            np.logical_or(
+                in_bounds,
+                np.logical_and(x_min <= x, x <= x_max),
+                out=in_bounds)
+        return in_bounds
+
     def with_function_outside_plateaus(self, f):
         return self.__class__(self.heights, self.x_clusters, f)
+
+    def for_training(self, x, stddev=0.0):
+        y = self(np.squeeze(x)).astype('float32')
+
+        ge = self.in_bounds(x)
+        be = np.logical_not(ge)
+
+        gdata = Data(x[ge], np.expand_dims(y[ge], axis=1))
+        bdata = Data(x[be], np.expand_dims(y[be], axis=1))
+        data = NamedDataSets(good=gdata, bad=bdata)
+
+        noisy_y = np.expand_dims(
+            self(np.squeeze(x), stddev=stddev), axis=1).astype('float32')
+
+        gdata = Data(x[ge], noisy_y[ge])
+        bdata = Data(x[be], noisy_y[be])
+        noisy_data = NamedDataSets(good=gdata, bad=bdata)
+
+        combined_raw_data = data.all()
+        sort_indices = combined_raw_data.phi.numpy().argsort(axis=0).squeeze()
+
+        return DataComponentsForTraining(data, noisy_data, combined_raw_data,
+                                         sort_indices)
