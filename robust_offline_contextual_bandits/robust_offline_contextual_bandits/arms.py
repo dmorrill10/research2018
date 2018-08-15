60149f052c5df9e454b7bf8af602fefe9dc17a2f
Dustin Morrill <dmorrill10@gmail.com>
2018-08-14 21:56:36 -0600

    Improve `Arms` and `ArmsWithContexts` interfaces.

--- robust_offline_contextual_bandits/robust_offline_contextual_bandits/arms.py
+++ robust_offline_contextual_bandits/robust_offline_contextual_bandits/arms.py
@@ -16,15 +16,35 @@ class Arms(object):
     def __len__(self):
         return len(self.reward_functions)
 
+    def with_function_outside_plateaus(self, f):
+        return self.__class__(
+            [
+                r.with_function_outside_plateaus(f)
+                for r in self.reward_functions
+            ],
+            colors=self.colors)
+
 
 class ArmsWithContexts(object):
     def __init__(self, arms, x, stddev=0.0):
         self.arms = arms
         self.x = x
+        self.stddev = stddev
         self.components_for_training = arms.components_for_training(x, stddev)
+
+    def __getitem__(self, action):
+        return self.components_for_training[action]
+
+    def combined_raw_y(self, action):
+        si = self[action].sort_indices
+        return self[action].combined_raw_data.y.numpy()[si, :]
 
     def num_contexts(self):
         return len(self.x)
 
     def num_arms(self):
         return len(self.arms)
+
+    def with_function_outside_plateaus(self, f):
+        return self.__class__(
+            self.arms.with_function_outside_plateaus(f), self.x, self.stddev)
