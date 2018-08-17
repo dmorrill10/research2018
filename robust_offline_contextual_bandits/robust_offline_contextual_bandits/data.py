0145e4e6592de8fdbf578f701d67f1e7045b9888
Dustin Morrill <dmorrill10@gmail.com>
2018-08-17 16:58:54 -0600

    Add `mean_and_t_ci`.

--- robust_offline_contextual_bandits/robust_offline_contextual_bandits/data.py
+++ robust_offline_contextual_bandits/robust_offline_contextual_bandits/data.py
@@ -1,5 +1,12 @@
 from collections import namedtuple
+import scipy.stats as st
 
 DataComponentsForTraining = namedtuple(
     'DataComponentsForTraining',
     ['data', 'noisy_data', 'combined_raw_data', 'sort_indices'])
+
+
+def mean_and_t_ci(a, axis=-1, confidence=0.95):
+    sem = st.sem(a, axis=axis)
+    prob_mass = st.t.ppf((1.0 + confidence) / 2.0, a.shape[axis] - 1.0)
+    return a.mean(axis=axis), sem * prob_mass
