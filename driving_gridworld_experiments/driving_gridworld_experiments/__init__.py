658b64cef3cff08368ff4fbe9e534c7ac80ac719
Dustin Morrill <dmorrill10@gmail.com>
2018-10-22 09:38:17 -0600

    Remove old functions and add `new_road`.

--- driving_gridworld_experiments/driving_gridworld_experiments/__init__.py
+++ driving_gridworld_experiments/driving_gridworld_experiments/__init__.py
@@ -1,30 +1,19 @@
-import tensorflow as tf
+from driving_gridworld.road import Road
+from driving_gridworld.car import Car
+from driving_gridworld.obstacles import Bump, Pedestrian
 
 
-def state_idx_map(transitions_dict):
-    state_indices = {}
-    idx = 0
-    for state in transitions_dict.keys():
-        state_indices[state] = idx
-        idx += 1
-    assert len(state_indices) == len(transitions_dict)
-    assert idx == len(transitions_dict)
-    return state_indices
-
-
-def test_state_idx_map(transitions):
-    _patient = state_idx_map(transitions)
-    assert len(_patient) == len(transitions)
-    assert len(_patient) == len(transitions)
-
-    max_idx = len(_patient)
-    _patient = {
-        k: tf.one_hot(idx, depth=max_idx)
-        for k, idx in _patient.items()
-    }
-    assert len(_patient) == len(transitions)
-
-
-if __name__ == '__main__':
-    raise NotImplementedError()
-    # test_state_idx_map(transitions)
+def new_road(headlight_range=3,
+             allow_crashing=False,
+             car=None,
+             obstacles=None,
+             allowed_obstacle_appearance_columns=None):
+    return Road(
+        headlight_range,
+        Car(2, 0) if car is None else car,
+        obstacles=([Bump(-1, -1), Pedestrian(-1, -1, speed=1)]
+                   if obstacles is None else obstacles),
+        allowed_obstacle_appearance_columns=(
+            [{2}, {1}] if allowed_obstacle_appearance_columns is None else
+            allowed_obstacle_appearance_columns),
+        allow_crashing=allow_crashing)
