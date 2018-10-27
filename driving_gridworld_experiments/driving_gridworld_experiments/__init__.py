91c920083a093a935d327ead461ed3b6921a6d5f
Dustin Morrill <dmorrill10@gmail.com>
2018-10-26 19:34:43 -0600

    Increase the car's probability of appearing.

--- driving_gridworld_experiments/driving_gridworld_experiments/__init__.py
+++ driving_gridworld_experiments/driving_gridworld_experiments/__init__.py
@@ -18,8 +18,8 @@ def new_road(headlight_range=3,
         headlight_range,
         Car(2, 0) if car is None else car,
         obstacles=([
-            Bump(-1, -1),
-            Pedestrian(-1, -1, speed=1, prob_of_appearing=0.1)
+            Bump(-1, -1, prob_of_appearing=0.2),
+            Pedestrian(-1, -1, speed=1, prob_of_appearing=0.2)
         ] if obstacles is None else obstacles),
         allowed_obstacle_appearance_columns=(
             [{2}, {1}] if allowed_obstacle_appearance_columns is None else
