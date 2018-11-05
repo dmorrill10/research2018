a253aa123d9e5519d26d466382568b3c1907e46b
Dustin Morrill <dmorrill10@gmail.com>
2018-11-05 12:02:16 -0700

    Add and improve action sampling functions.

--- research2018/research2018/__init__.py
+++ research2018/research2018/__init__.py
@@ -1,6 +1,12 @@
 import tensorflow as tf
+import numpy as np
 
 
-def sample_action(policy):
-    dist = tf.distributions.Categorical(probs=policy)
-    return dist.sample(1)
+def sample_action_tf(policy, num_samples=1):
+    return tf.distributions.Categorical(probs=policy).sample(num_samples)
+
+
+def sample_action_np(policy_at_cur_state, num_samples=1):
+    size = [num_samples] if num_samples > 1 else []
+    return np.random.choice(
+        a=range(len(policy_at_cur_state)), size=size, p=policy_at_cur_state)
