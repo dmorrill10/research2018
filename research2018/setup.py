bc052f266d878a1ce7b811c4fa09c1f471806a57
Dustin Morrill <dmorrill10@gmail.com>
2020-11-09 10:24:30 -0700

    Update configuration

--- research2018/setup.py
+++ research2018/setup.py
@@ -5,7 +5,11 @@ setup(
     version='0.0.1',
     license='',
     packages=find_packages(),
-    install_requires=['setuptools >= 20.2.2'],
+    install_requires=[
+        'setuptools >= 20.2.2',
+        'tensorflow >= 2.0',
+        # 'tf-contextual_prediction_with_expert_advice',  # not on pip
+    ],
     tests_require=['pytest', 'pytest-cov'],
     setup_requires=['pytest-runner'],
 )
